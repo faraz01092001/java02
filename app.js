d5833d81bd0d6e46e9fa3365daa464fc129dd075
faraz01092001 <syerfaraz123@gmail.com>
2025-02-17 18:31:15 +0500

    Java

---
+++ app.js
@@ -0,0 +1,60 @@
+// Home Assignment
+// Chapter no 06 math expression II (start)
+
+// Question no 01 
+var x = x;
+var x = x++;
+
+// question no 02
+var x = 100;
+var x = --x;
+
+// question no 03
+var x = 50;
+var y = x++;
+    y = 50;
+
+// question no 04 
+var y = 50;
+var z = --y;
+    z = 49;
+
+ // question no 05
+ var newNum = num--;   
+
+ // question no 06
+ var x = x++
+
+ // question no 07
+ var num = 5;
+ var num2 = num++;
+ alert(num++);
+
+ // Chapter no 06 math expression II (End)
+
+ // Chapter no 07 math expression III (start)
+
+ // Chapter no 01
+ var num = 2+ (2*6);
+ num = 14;
+ 
+ // Chapter no 02
+ var num = (2+2)*6;
+  num = 24;
+
+// Chapter no 03
+var num = (2+2)*(4+2);
+ num = 24;
+
+// Chapter no 04
+var num = ((2+2)*4)+2;
+ num = 18;
+
+// Chapter no 05
+var num = ((5+9)*3)+14;
+num = 56;
+
+// Chapter no 06
+var num = ((2+2)*3)+8;
+num = 20
+
