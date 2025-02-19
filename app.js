ea5d4fac56968bcf7d39baf0adcdee9525b137db
faraz01092001 <syerfaraz123@gmail.com>
2025-02-19 23:20:03 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,59 +2,132 @@
 // Chapter no 06 math expression II (start)
 
 // Question no 01 
-var x = x;
-var x = x++;
+// var x = x;
+// var x = x++;
 
 // question no 02
-var x = 100;
-var x = --x;
+// var x = 100;
+// var x = --x;
 
 // question no 03
-var x = 50;
-var y = x++;
-    y = 50;
+// var x = 50;
+// var y = x++;
+//     y = 50;
 
 // question no 04 
-var y = 50;
-var z = --y;
-    z = 49;
+// var y = 50;
+// var z = --y;
+//     z = 49;
 
  // question no 05
- var newNum = num--;   
+//  var newNum = num--;   
 
  // question no 06
- var x = x++
+//  var x = x++
 
  // question no 07
- var num = 5;
- var num2 = num++;
- alert(num++);
+//  var num = 5;
+//  var num2 = num++;
+//  alert(num++);
 
  // Chapter no 06 math expression II (End)
 
  // Chapter no 07 math expression III (start)
 
- // Chapter no 01
- var num = 2+ (2*6);
- num = 14;
+ // question no 01
+//  var num = 2+ (2*6);
+//  num = 14;
  
- // Chapter no 02
- var num = (2+2)*6;
-  num = 24;
+ // question no 02
+//  var num = (2+2)*6;
+//   num = 24;
 
-// Chapter no 03
-var num = (2+2)*(4+2);
- num = 24;
+// question no 03
+// var num = (2+2)*(4+2);
+//  num = 24;
 
-// Chapter no 04
-var num = ((2+2)*4)+2;
- num = 18;
+// question no 04
+// var num = ((2+2)*4)+2;
+//  num = 18;
 
-// Chapter no 05
-var num = ((5+9)*3)+14;
-num = 56;
+// question no 05
+// var num = ((5+9)*3)+14;
+// num = 56;
 
-// Chapter no 06
-var num = ((2+2)*3)+8;
-num = 20
+// question no 06
+// var num = ((2+2)*3)+8;
+// num = 20
 
+// question no 07
+// var pressure; 
+// pressure = (4 / 2) * 4; 
+// console.log(pressure); // Output: 8
+
+// Chapter 8  start (Concatenating Text Strings)
+
+// question no 01
+// var num = "2" + "2";
+// console.log(num); Output: 22
+
+// question no 02
+// var messge = ("Hello Dolly");
+//     console.log("message");
+
+// question no 03
+// var num = "33";  
+// var value = 3;   
+// var result = num + value;  // Concatenation hoga (string + number)
+
+// alert(result);  // Alert box me "333" show hoga
+
+// question no 04
+// var part1 = "Pakistan";  
+// var part2 = " Zindabad"; 
+// var message = part1 + part2;  // Dono strings ko concatenate kiya
+
+// alert(message);  // Alert box me "Pakistan Zindabad" show hoga
+
+// / question no 05
+// var text = "Hello";  
+// var number = 123;    
+// var result = text + number;  // Concatenation (String + Number)
+
+// alert(result);  // Alert box me "Hello123" show hoga
+
+// / question no 06
+// var firstName = "Faraz";
+// var lastName = " Ahmed";  
+// var fullName = firstName + lastName;  // Dono strings concatenate ki gayi
+
+// alert(fullName);  // Alert box me "Faraz Ahmed" show hoga
+
+
+// Chapter 9 start (Prompts)
+// / / question no 01
+// var firstName = prompt("Enter first name");  
+
+// console.log(firstName);  
+
+// / / question no 02
+// var country = prompt("Country?", "China");  
+
+// console.log(country);  
+
+
+// / / question no 03
+// var yourName = prompt("Enter Your Name");  
+
+// console.log(yourName);  
+
+// / / question no 04
+// var message = "Enter your city name:";  
+// var defaultResponse = "Karachi";
+
+// var userCity = prompt(message, defaultResponse); 
+
+// console.log(userCity);  
+
+// / / question no 05
+// var userResponse = prompt("Enter your age:");  
+
+// console.log(userResponse);  
